e2cb22ed12a65f1f01cf661d9314031c5b8dc029
Matthew McNamee <61328700+TongueTwistinTony@users.noreply.github.com>
2020-03-18 23:19:16 -0600

    Update Box.py

--- MVMShapez/Box.py
+++ MVMShapez/Box.py
@@ -1,1 +1,19 @@
+class Box():
+    def __init__(self, w, l, h):
+        self.w = w
+        self.l = l
+        self.h = h
 
+    def getVolume(self):
+      l = self.l
+      w = self.w
+      h = self.h
+      volume = str(l*w*h)
+      return volume
+        
+    def getSurfaceArea(self):
+      l = self.l
+      w = self.w
+      h = self.h
+      surfaceArea = str(2*l*w + 2*h*w + 2*h*l)
+      return surfaceArea
