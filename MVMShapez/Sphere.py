81f8b0df492b6fe04243abf0b9bfd124c1305993
Matthew McNamee <61328700+TongueTwistinTony@users.noreply.github.com>
2020-03-18 23:20:50 -0600

    Update Sphere.py

--- MVMShapez/Sphere.py
+++ MVMShapez/Sphere.py
@@ -1,1 +1,16 @@
+import math
 
+class Sphere():
+    pi = 3.14159265
+    def __init__(self, r):
+        self.r = r
+
+    def getVolume(self):
+      r = self.r
+      volume = str((4/3)*math.pi*r*r*r)
+      return volume
+
+    def getSurfaceArea(self):
+      r = self.r
+      surfaceArea = str(4*math.pi*r*r)
+      return surfaceArea
